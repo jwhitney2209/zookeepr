6397f58b8578dd3fc43ed75aac444a997a4dc263
teachergg <jwhitney@bisd.us>
2022-04-16 16:19:47 -0500

    added zookeeper form handling and filters to the front end :

---
+++ __tests__/animals.test.js
@@ -0,0 +1,92 @@
+const fs = require("fs");
+const {
+  filterByQuery,
+  findById,
+  createNewAnimal,
+  validateAnimal,
+} = require("../lib/animals.js");
+const { animals } = require("../data/animals");
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+
+jest.mock('fs');
+
+test('create an animal object', () => {
+  const animal = createNewAnimal({
+    name: 'Darlene',
+    id: '0023415'
+  },
+  animals);
+
+  expect(animal.name).toBe('Darlene');
+  expect(animal.id).toBe('0023415');
+
+});
+
+test('filter by query', () => {
+  const startingAnimals = [
+    {
+      id: "3",
+      name: "Erica",
+      species: "gorilla",
+      diet: "omnivore",
+      personalityTraits: ["quirky", "rash"],
+    },
+    {
+      id: "4",
+      name: "Noel",
+      species: "bear",
+      diet: "carnivore",
+      personalityTraits: ["impish", "sassy", "brave"],
+    },
+  ];
+
+  const updatedAnimals = filterByQuery({ species: "gorilla" }, startingAnimals);
+
+  expect(updatedAnimals.length).toEqual(1);
+});
+
+test('find by id', () => {
+  const startingAnimals = [
+    {
+      id: "3",
+      name: "Erica",
+      species: "gorilla",
+      diet: "omnivore",
+      personalityTraits: ["quirky", "rash"],
+    },
+    {
+      id: "4",
+      name: "Noel",
+      species: "bear",
+      diet: "carnivore",
+      personalityTraits: ["impish", "sassy", "brave"],
+    },
+  ];
+
+  const result = findById("3", startingAnimals);
+
+  expect(result.name).toBe("Erica");
+});
+
+test('validate animal personality traits', () => {
+  const animal = {
+    id: "3",
+    name: "Erica",
+    species: "gorilla",
+    diet: "omnivore",
+    personalityTraits: ["quirky", "rash"],
+  };
+
+  const invalidAnimal = {
+    id: "3",
+    name: "Erica",
+    species: "gorilla",
+    diet: "omnivore",
+  };
+
+  const result = validateAnimal(animal);
+  const result2 = validateAnimal(invalidAnimal);
+
+  expect(result).toBe(true);
+  expect(result2).toBe(false);
+});
